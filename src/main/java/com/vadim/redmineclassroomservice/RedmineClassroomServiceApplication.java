96d7ac6c492f6df0020dddc53978f42ce0240ebe
Vadim Babikov <b.vadim.alexeyevich@gmail.com>
2023-08-20 18:55:47 +0700

    initialized project

---
+++ src/main/java/com/vadim/redmineclassroomservice/RedmineClassroomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vadim.redmineclassroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedmineClassroomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedmineClassroomServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vadim/redmineclassroomservice/RedmineClassroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadim.redmineclassroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmineClassroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
