a92d046f72120f9314fc81e4275eb453ed967562
Vadim Babikov <b.vadim.alexeyevich@gmail.com>
2023-08-20 23:17:39 +0700

    added authentication

--- src/test/java/com/vadim/redmineclassroomservice/HomeControllerTest.java
+++ src/test/java/com/vadim/redmineclassroomservice/HomeControllerTest.java
@@ -3,6 +3,7 @@ package com.vadim.redmineclassroomservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -16,7 +17,8 @@ class HomeControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void shouldDisplayHome() throws Exception {
+    @WithMockUser
+    void authenticatedUserShouldDisplayHomePage() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
